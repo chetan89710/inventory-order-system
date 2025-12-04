34c40c1255e6a8bbce8a653af0680b8b31f501c5
chetan89710 <chetan89710@gmail.com>
2025-12-04 18:17:26 +0530

    Updated Users module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,15 @@
+// src/users/users.module.ts
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { UsersSeed } from './users.seed';
 
-@Module({})
-export class UsersModule {}
+@Module({
+  imports: [SequelizeModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService, UsersSeed],
+  exports: [UsersService],
+})
+export class UsersModule { }
