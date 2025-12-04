f084df797fdab7dd3f2ffc8f14d6c1cc390dcb82
chetan89710 <chetan89710@gmail.com>
2025-12-04 18:17:37 +0530

    Updated Products module

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { Product } from './entities/product.entity';
+import { S3Service } from '../aws/s3/s3.service';
 
-@Module({})
-export class ProductsModule {}
+@Module({
+  imports: [SequelizeModule.forFeature([Product])],
+  providers: [ProductsService, S3Service],
+  controllers: [ProductsController],
+})
+export class ProductsModule { }
